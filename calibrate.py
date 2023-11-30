97e485491e4bf5712acce86429a3eef8ab041466
perrintong <perrintong@icloud.com>
2023-11-30 01:07:30 -0500

    picam modifciations

--- calibrate.py
+++ calibrate.py
@@ -9,24 +9,18 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import matplotlib.pyplot as plt
 
-def calibrate():
-    imageL = cv2.cvtColor(cv2.VideoCapture(0), cv2.COLOR_BGR2GRAY)
-    imageR = cv2.cvtColor(cv2.VideoCapture(1), cv2.COLOR_BGR2GRAY)
+def calibrate(image):
+    # image = cv2.cvtColor(cv2.VideoCapture(0), cv2.COLOR_BGR2GRAY)
     atDetector = Detector(families="tag36h11", nthreads=1,
                           quad_decimate=1.0, quad_sigma=0.0,
                           refine_edges=1, decode_sharpening=0.25, debug=0)
     mtx = np.loadtxt('cam_intrinsics.txt')
-    camera_params = (mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2])
-    resultsL = atDetector.detect(imageL, estimate_tag_pose=True, 
-                                 camera_params=camera_params, tag_size=0.249)
-    resultsR = atDetector.detect(imageR, estimate_tag_pose=True,
-                                 camera_params=camera_params, tag_size=0.249)
     zeros = np.zeros((mtx.shape[0], 1))
     mtx2 = np.hstack((mtx, zeros))
-    extrinsicsL = np.vstack((np.hstack((resultsL[0].pose_R, resultsL[0].pose_t)), [0,0,0,1]))
-    extrinsicsR = np.vstack((np.hstack((resultsR[0].pose_R, resultsR[0].pose_t)), [0,0,0,1]))
-    extrinsicsL = np.linalg.inv(extrinsicsL)
-    extrinsicsR = np.linalg.inv(extrinsicsR)
-    projectionL = np.matmul(mtx2, extrinsicsL)
-    projectionR = np.matmul(mtx2, extrinsicsR)
-    return projectionL, projectionR
+    camera_params = (mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2])
+    results = atDetector.detect(image, estimate_tag_pose=True, 
+                                camera_params=camera_params, tag_size=0.249)
+    extrinsics = np.vstack((np.hstack((results[0].pose_R, results[0].pose_t)), [0,0,0,1]))
+    extrinsics = np.linalg.inv(extrinsics)
+    projection = np.matmul(mtx2, extrinsics)
+    return projection
---
+++ detection.py
@@ -0,0 +1,67 @@
+import cv2
+from dt_apriltags import Detector
+import numpy as np
+import os
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import matplotlib.pyplot as plt
+import socket
+import pickle
+from calibrate import calibrate
+from picamera2 import Picamera2
+import time
+
+# s = socket.socket()
+# port = 517
+# addr = '192.168.188.0.1'
+# s.connect((addr, port))
+
+def detection():
+    picam = Picamera2()
+    config = picam.create_video_configuration({"size": (1920, 1080)})
+    picam.configure(config)
+    picam.start()
+    # projection = calibrate(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    base_options = python.BaseOptions(model_asset_path='final/pose_landmarker_lite.task')
+    options = vision.PoseLandmarkerOptions(base_options=base_options, 
+                                       output_segmentation_masks=True)
+    detector = vision.PoseLandmarker.create_from_options(options)
+
+    while True:
+        start_time = time.time()
+        image = picam.capture_array("main")
+        image = cv2.rotate(image, cv2.ROTATE_180)
+        status = True
+        if status:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            imagemp = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            detection_result = detector.detect(imagemp)
+            resultscoords = []
+            if detection_result.pose_landmarks:
+                for x in range(0, 33):
+                    pose = [detection_result.pose_landmarks[0][x].x * imagemp.width,
+                            detection_result.pose_landmarks[0][x].y * imagemp.height]
+                    resultscoords.append(pose)
+                    cv2.circle(image, (int(pose[0]), int(pose[1])), 5, (0, 255, 0), -1)
+                resultscoords = (np.array(resultscoords)).T
+            else:
+                resultscoords = np.full((2, 33), -1)
+            # data = pickle.dumps([projection, resultscoords])
+            # s.send(data)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # cv2.imshow('Webcam Footage', image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            print("camera not working")
+        time_elapsed = time.time() - start_time
+        if time_elapsed < 1./60:  # we aim for 30 fps
+            time.sleep(1./60 - time_elapsed)
+
+if __name__ == "__main__":
+    detection()
+    # s.close()
+    cv2.destroyAllWindows()
